b197b59e5449de976187b15b9f3b9b44d3212fcb
FEA-eng <59876896+FEA-eng@users.noreply.github.com>
2026-04-29 18:26:00 +0200

    FEM: fix * placement in the header
    fixes the * position in the header

--- src/Mod/Fem/femexamples/ccx_1D_fluid_network.py
+++ src/Mod/Fem/femexamples/ccx_1D_fluid_network.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 # ***************************************************************************
-# *   Copyright (c) 2026 Jakub Michalski <jakub.j.michalski[at]gmail.com>         *
+# *   Copyright (c) 2026 Jakub Michalski <jakub.j.michalski[at]gmail.com>   *
 # *                                                                         *
 # *   This file is part of FreeCAD.                                         *
 # *                                                                         *
